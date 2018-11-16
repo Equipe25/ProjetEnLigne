12030981856b4987d52c8b4fafcaf006a6e3be51
hhaml <hakim.hamlat.1@ulaval.ca>
2018-11-16 14:27:35 -0500

    projet dans github testaa

--- src/amzevent/Domaine/LarmanController.java
+++ src/amzevent/Domaine/LarmanController.java
@@ -11,7 +11,6 @@ import amzevent.Domaine.SectionAssise;
 import amzevent.Domaine.Util.Vector2F;
 import java.util.ArrayList;
 import javax.swing.ListModel;
-//test
 public class LarmanController {
     private Salle salle;
     private Scene scene;
